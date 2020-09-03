00d6f768a473e40537963a2a277b082c74e60f61
jasoncg <jasonxgr@gmail.com>
2020-09-03 23:04:39 +0300

    Adding firebase

---
+++ slack-app/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwK7f4EorBBQG-nmhoHP-_dk8QVot6mpE",
+    authDomain: "slack-alike.firebaseapp.com",
+    databaseURL: "https://slack-alike.firebaseio.com",
+    projectId: "slack-alike",
+    storageBucket: "slack-alike.appspot.com",
+    messagingSenderId: "511250835303",
+    appId: "1:511250835303:web:3a4bae0e8ef3785e5131c2",
+    measurementId: "G-HCT1QK0WB4"
+  };
