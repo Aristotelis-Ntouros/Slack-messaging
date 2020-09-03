9a2e9d3e34f04ae99478decef6a8dbbd682e0728
jasoncg <jasonxgr@gmail.com>
2020-09-03 23:11:55 +0300

    Adding firebase import, export

--- slack-app/src/firebase.js
+++ slack-app/src/firebase.js
@@ -1,3 +1,6 @@
+
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAwK7f4EorBBQG-nmhoHP-_dk8QVot6mpE",
     authDomain: "slack-alike.firebaseapp.com",
@@ -8,3 +11,11 @@ const firebaseConfig = {
     appId: "1:511250835303:web:3a4bae0e8ef3785e5131c2",
     measurementId: "G-HCT1QK0WB4"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firestore.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
